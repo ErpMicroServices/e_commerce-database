d163cb67920129c1e66735da73021b4d0c3655e4
Jim Barrows <Jim.Barrows@gmail.com>
2017-02-28 08:41:22 -0700

    Ignoring node_modules added more tables.

--- migrations/sqls/20170208162026-1-0-0-down.sql
+++ migrations/sqls/20170208162026-1-0-0-down.sql
@@ -10,6 +10,11 @@ alter table party_object drop constraint if exists party_object_web_object_id_fk
 
 alter table product_object drop constraint if exists product_object_web_object_id_fkey;
 
+alter table subscription drop constraint if exists subscription_subscription_type_id_fk;
+
+alter table subscription_fulfillment_piece drop constraint if exists subscription_fulfillment_piece_subscription_id_fkey;
+alter table subscription_fulfillment_piece drop constraint if exists subscription_fulfillment_piece_subscription_activity_id_fkey;
+
 alter table web_content_association drop constraint if exists web_content_association_function_type_fkey;
 alter table web_content_association drop constraint if exists web_content_association_associate_for_fkey;
 alter table web_content_association drop constraint if exists web_content_association_associate_of_fkey;
@@ -21,14 +26,21 @@ alter table web_object drop constraint if exists web_object_web_object_type_id_f
 
 alter table web_user_preference drop constraint if exists web_user_preference_web_preference_type_id_fkey;
 
+drop table if exists electronic_text_object;
 drop table if exists feature_object;
 drop table if exists function_type;
+drop table if exists image_object;
 drop table if exists login_account_history;
 drop table if exists object_purpose;
 drop table if exists object_usage;
+drop table if exists other_object;
 drop table if exists party_object;
 drop table if exists product_object;
 drop table if exists purpose_type;
+drop table if exists subscription;
+drop table if exists subscription_activity;
+drop table if exists subscription_fulfillment_piece;
+drop table if exists subscription_type;
 drop table if exists user_login;
 drop table if exists web_content;
 drop table if exists web_content_association;
--- migrations/sqls/20170208162026-1-0-0-up.sql
+++ migrations/sqls/20170208162026-1-0-0-up.sql
@@ -89,6 +89,19 @@ create table if not exists web_object (
   constraint web_object_id_pk primary key(id)
 );
 
+create table if not exists electronic_text_object(
+  electronic_text text not null constraint electronic_text_object_electronic_text_not_empty check(electronic_text <> '')
+) inherits (web_object);
+
+create table if not exists image_object(
+  image_location text not null constraint image_object_image_location_not_empty check(image_location <> '')
+) inherits (web_object);
+
+create table if not exists other_object(
+  object_content text,
+  object_location text
+) inherits (web_object);
+
 create table if not exists feature_object(
   id uuid default uuid_generate_v4(),
   product_feature_id uuid not null,
@@ -130,4 +143,39 @@ create table if not exists party_object(
   party_id uuid not null,
   web_object_id uuid not null references web_object(id),
   constraint party_object_pk primary key(id)
+);
+
+create table if not exists subscription_type(
+  id uuid default uuid_generate_v4(),
+  description text not null unique constraint subscription_type_description_not_empty check(description <> ''),
+  constraint subscription_type_pk primary key(id)
+);
+
+create table if not exists subscription(
+  id uuid default uuid_generate_v4(),
+  start_date timestamp not null,
+  end_date timestamp,
+  subscription_type_id uuid not null references subscription_type(id),
+  product_id uuid,
+  product_category_id uuid,
+  need_type uuid,
+  party_role_id uuid,
+  contact_mechanism_id uuid,
+  commmunication_event_id uuid,
+  party_need uuid,
+  constraint subscription_pk primary key(id)
+);
+
+create table if not exists subscription_activity(
+  id uuid default uuid_generate_v4(),
+  date_sent timestamp not null,
+  comment text,
+  constraint subscription_activity_pk primary key(id)
+);
+
+create table if not exists subscription_fulfillment_piece(
+  id uuid default uuid_generate_v4(),
+  subscription_id uuid not null references subscription(id),
+  subscription_activity_id uuid not null references subscription_activity(id),
+  constraint subscription_fulfillment_piece_pk primary key(id)
 );
