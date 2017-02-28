8d17a04e919575e5bcb4e6e4e7112ba3f525460f
Jim Barrows <Jim.Barrows@gmail.com>
2017-02-28 07:13:21 -0700

    Added more tables for e-commerce

--- migrations/sqls/20170208162026-1-0-0-down.sql
+++ migrations/sqls/20170208162026-1-0-0-down.sql
@@ -1,4 +1,17 @@
+alter table web_content_association drop constraint if exists web_content_association_function_type_fkey;
+alter table web_content_association drop constraint if exists web_content_association_associate_for_fkey;
+alter table web_content_association drop constraint if exists web_content_association_associate_of_fkey;
+
+alter table web_content_role drop constraint if exists web_content_role_web_content_id_fkey;
+alter table web_content_role drop constraint if exists web_content_role_web_content_type_id_fkey;
+
+alter table web_user_preference drop constraint if exists web_user_preference_web_preference_type_id_fkey;
+
+drop table if exists function_type;
 drop table if exists login_account_history;
 drop table if exists user_login;
-drop table if exists web_user_preference;
+drop table if exists web_content;
+drop table if exists web_content_association;
+drop table if exists web_content_status_type;
 drop table if exists web_preference_type;
+drop table if exists web_user_preference;
--- migrations/sqls/20170208162026-1-0-0-up.sql
+++ migrations/sqls/20170208162026-1-0-0-up.sql
@@ -12,8 +12,8 @@ create table if not exists login_account_history(
   user_login uuid not null references user_login(id),
   user_id text not null  unique constraint user_id check (user_id <> ''),
   password text constraint password_not_empty check (password <> ''),
-  from_date timestamp not null,
-  thru_date timestamp,
+  active_from timestamp not null,
+  active_thru timestamp,
   constraint login_account_history_id_pk primary key(id)
 );
 
@@ -27,4 +27,49 @@ create table if not exists web_user_preference(
   id uuid default uuid_generate_v4(),
   web_preference_type_id uuid not null references web_preference_type(id),
   constraint web_user_preference_id_pk primary key(id)
+);
+
+create table if not exists web_content_status_type(
+  id uuid default uuid_generate_v4(),
+  description text not null constraint  web_content_status_type_description_not_empty check( description <> ''),
+  constraint web_content_status_type_id_pk primary key(id)
+);
+
+create table if not exists web_content(
+  id uuid default uuid_generate_v4(),
+  content_description text not null constraint  content_description_not_empty check( content_description <> ''),
+  file_location text,
+  web_content_status_type_id uuid not null,
+  constraint web_content_id_pk primary key(id)
+  );
+
+create table if not exists function_type(
+  id uuid default uuid_generate_v4(),
+  content_description text not null constraint  content_description_not_empty check( content_description <> ''),
+  constraint function_type_id_pk primary key(id)
+);
+
+create table if not exists web_content_association(
+  id uuid default uuid_generate_v4(),
+  upper_left_coordinate point,
+  function_type uuid references function_type(id),
+  associate_for uuid references web_content(id),
+  associate_of uuid references web_content(id),
+  constraint web_content_association_id_pk primary key(id)
+);
+
+create table if not exists web_content_type(
+  id uuid default uuid_generate_v4(),
+  content text not null unique,
+  constraint web_content_type_id_pk primary key(id)
+);
+
+create table if not exists web_content_role(
+  id uuid default uuid_generate_v4(),
+  active_from timestamp not null,
+  active_thru timestamp,
+  web_content_type_id uuid references web_content_type(id),
+  web_content_id uuid references web_content(id),
+  party_id uuid,
+  constraint web_content_role_id_pk primary key(id)
 );
